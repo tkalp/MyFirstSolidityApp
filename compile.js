87f9fe426e00fbd63b398d9c7ee8816c35f8fd7d
unknown <teddy.kalp@gmail.com>
2021-11-04 20:26:34 -0600

    added code to compiler

--- compile.js
+++ compile.js
@@ -1,1 +1,10 @@
-// compile code will go here
+const path = require('path'); // Used to get the path of the current directory 
+const fs = require('fs'); // Filereader
+const solc = require('solc'); // Solidity Compiler
+
+
+const inboxPath = path.resolve(__dirname, 'contracts', 'Inbox.sol'); // Get the path of the Inbox.sol
+const source = fs.readFileSync(inboxPath, 'utf8');
+
+console.log(solc.compile(source,1));
+
