295b20809e456753e3772640a23f06d3dacc6393
unknown <teddy.kalp@gmail.com>
2021-11-04 21:29:10 -0600

    deploying contract

--- compile.js
+++ compile.js
@@ -2,9 +2,8 @@ const path = require('path'); // Used to get the path of the current directory
 const fs = require('fs'); // Filereader
 const solc = require('solc'); // Solidity Compiler
 
-
 const inboxPath = path.resolve(__dirname, 'contracts', 'Inbox.sol'); // Get the path of the Inbox.sol
-const source = fs.readFileSync(inboxPath, 'utf8');
+const source = fs.readFileSync(inboxPath, 'utf8'); // Filereader
 
-console.log(solc.compile(source,1));
+module.exports = solc.compile(source,1).contracts[':Inbox']; // Compile the code and get the specific contract, in this case 'Inbox'
 
--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -1,1 +1,42 @@
-// contract test code will go here
+const assert = require('assert'); // a service used to make assertions about test
+const ganache = require('ganache-cli') // serve as local ETH test network
+
+// Web3 is portal to ETH programming
+// Has Versioning issues 
+// 0.x.x - Primitive interface, only callbacks for async code
+// 1.x.x = Support for promises + async/wai
+const Web3 = require('web3');
+// Create instance of Web3
+const web3 = new Web3(ganache.provider());
+const {interface, bytecode} = require('../compile');
+
+
+
+// Declare global array of accounts to be populated
+let accounts;
+let inbox;
+
+beforeEach( async () => {
+    // Get a list of all accounts
+    accounts = await web3.eth.getAccounts();
+    // Use one of those accounts to deploy the contract
+    inbox = await new web3.eth.Contract(JSON.parse(interface))
+        .deploy({
+            data: bytecode,
+            arguments: ['Hello']
+        })
+        .send({
+            from: accounts[0], 
+            gas: '1000000'
+        })
+});
+
+// Run the tests
+describe('Inbox', () => {
+    it('deploys a contract', () => {
+        console.log(inbox);
+    });
+});
+
+
+
