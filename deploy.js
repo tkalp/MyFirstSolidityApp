a60a7f164ee98cfebc43094292973fddeed464ef
unknown <teddy.kalp@gmail.com>
2021-11-06 14:49:19 -0600

    added all the last pieces of code for deployment

--- deploy.js
+++ deploy.js
@@ -1,1 +1,46 @@
 // deploy code will go here
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+const Web3 = require('web3');
+const {interface, bytecode} = require('./compile.js');
+
+const mneumonic = 'tattoo pioneer maximum roof result tomato involve index strategy radar bike still';
+const api = 'https://rinkeby.infura.io/v3/96d56aba0a1141a69f484f3be6509343';
+
+const INITIAL_MESSAGE = 'Hello';
+
+
+const provider = new HDWalletProvider(
+    mneumonic, // Mneumonic Phrase
+    api // Node we want to connect to
+);
+
+const web3 = new Web3(provider);
+
+
+let accounts;
+let inbox;
+// call function so that we can use the 'async' keyword
+const deploy = async () => {
+    accounts = await web3.eth.getAccounts();
+
+    console.log('Attempting to deploy from account ', accounts[0]);
+
+    inbox = await new web3.eth.Contract(JSON.parse(interface))
+            .deploy({
+                data: bytecode,
+                arguments: [INITIAL_MESSAGE]
+            })
+            .send({
+                from: accounts[0], 
+                gas: '1000000'
+            });
+
+
+    console.log('Contract deployed to ', inbox.options.address);
+    provider.engine.stop();
+};
+
+deploy();
+
+
+
--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -9,13 +9,17 @@ const Web3 = require('web3');
 // Create instance of Web3
 const web3 = new Web3(ganache.provider());
 const {interface, bytecode} = require('../compile');
+const INITIAL_MESSAGE = 'Hello';
 
+
+const api = 'https://rinkeby.infura.io/v3/96d56aba0a1141a69f484f3be6509343';
 
 
 // Declare global array of accounts to be populated
 let accounts;
 let inbox;
 
+// This functon runs before each test
 beforeEach( async () => {
     // Get a list of all accounts
     accounts = await web3.eth.getAccounts();
@@ -23,19 +27,35 @@ beforeEach( async () => {
     inbox = await new web3.eth.Contract(JSON.parse(interface))
         .deploy({
             data: bytecode,
-            arguments: ['Hello']
+            arguments: [INITIAL_MESSAGE]
         })
         .send({
             from: accounts[0], 
             gas: '1000000'
-        })
+        });
 });
 
 // Run the tests
 describe('Inbox', () => {
     it('deploys a contract', () => {
-        console.log(inbox);
+        assert.ok(inbox.options.address); // Checks the address is okay of contrac
     });
+
+    it('has a default message', async () => {
+        const message = await inbox.methods.message().call(); // Calls the attribute of 'Inbox' -> no need to specify address, just retrieving data from DB
+        assert.equal(message, INITIAL_MESSAGE);
+    });
+
+    it('can change message', async () => {
+        const newMessage = 'New Message';
+        await inbox.methods.setMessage(newMessage).send({ // Send Message
+            from: accounts[0]
+        });
+
+        const message = await inbox.methods.message().call(); 
+        assert.equal(message, newMessage);
+    });
+
 });
 
 
